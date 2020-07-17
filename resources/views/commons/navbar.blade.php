8310a78bd487d32e1d159a01ce571e0b7618b296
kodai-sakamoto <sakamotowoks22@gmail.com>
2020-07-17 07:46:16 +0000

    login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,8 +1,13 @@
 <header>
-    <nav class="navbar navbar-expand-sm navbar-red bg-red">
-        <a class="navbar-brand" href="/">全国ラーメンランキング</a>
-        <li class="nav-item">{!! link_to_route('signup.get', '登録する', [], ['class' => 'nav-link']) !!}</li>
-                    <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
-    </nav>
-    
+    <div class="header-logo">
+        <h2><a href="/">全国ラーメンランキング</a></h2>
+        <ul class="header-login">
+            @if (Auth::check())
+                <li class="nav-item">{!! link_to_route('logout.get', 'ログアウト') !!}</li>
+            @else
+                <li class="nav-item">{!! link_to_route('signup.get', '登録') !!}</li>
+                <li class="nav-item">{!! link_to_route('login', 'ログイン') !!}</li>
+            @endif
+        </ul>
+    </div>
 </header>
